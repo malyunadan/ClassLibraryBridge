3aadfafd621906e46a65f8d895ed318bc99d1e12
Malyun <maa026@edu.zealand.dk>
2025-09-22 22:00:51 +0200

    Implemented MC class and unit tests for Price() and Vehicle()

---
+++ ClassLibraryBridge/MC.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryBridge
+{
+    public class MC
+    {
+        /// <summary>
+        /// property for license plate
+        /// property for date
+        /// summary>
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// constructor for license plate and date
+        public MC(string licenseplate, DateTime date)
+        {
+            Licenseplate = licenseplate;
+            Date = date;
+        }
+        /// <summary>
+        /// method to return price
+        /// method to return vehicle type
+        public double Price()
+        {
+            return 120.0;
+        }
+        public string Vehicle()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ TestProjectMC/MCUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibraryBridge;
+namespace TestProjectMC
+{
+    [TestClass]
+    public sealed class MCUnitTests
+    {
+        [TestMethod]
+        public void Price_ReturnsFixedValue()
+        {
+            //Arange
+            var mc = new MC("ABCM", DateTime.Now);
+            //Act
+            var price = mc.Price();
+            //Assert
+            Assert.AreEqual(120.0, price);
+        }
+        [TestMethod]
+        public void Vehicle_ReturnsMC()
+        {
+            //Arange
+            var mc = new MC("ABCM", DateTime.Now);
+            //Act
+            var vehicleType = mc.Vehicle();
+            //Assert
+            Assert.AreEqual("MC", vehicleType);
+        }
+    }
+}
